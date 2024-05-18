053213d4face73a7a0a974d0b0aa142a71ba0822
tapriandi <tapriandi@gmail.com>
2024-05-18 10:16:11 +0700

    day-10

---
+++ day-10/search.js
@@ -0,0 +1,41 @@
+import { data } from "./products.js";
+
+// const number = [2, 4, 13, 6, 2, 34, 7, 8, 12];
+// const res = number.indexOf(2);
+
+// const res = number.find(
+//     function(num) {
+//         return num > 10
+//     }
+// );
+// const res = number.find(num => num > 10);
+
+// const res = number.findIndex(num => num > 10);
+
+// const res = number.includes(13);
+
+// const res = number.filter(num => num > 20);
+
+// console.log(res);
+
+
+// ===== linear search :: cek 1 demi 1 dari tiap data =====
+let keySearch = "iPhone";
+const products = data.products;
+
+// manual 
+// for (let i = 0; i < products.length; i++) {
+//   //   if (products[i].title.toLocaleLowerCase() == keySearch.toLocaleLowerCase()) {
+//   //     // console.log(products[i].title);
+//   //     console.log('datanya ada')
+//   //   }
+
+//   if (products[i].title.includes(keySearch)) {
+//     console.log(products[i]);
+//   }
+// }
+
+// js method
+// const res = products.filter(num => num.title === keySearch);
+const res = products.filter(num => num.title.includes(keySearch));
+console.log(res.length);
--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-// TEST CASES
+// let a = 123
 
-function dashWord() {
-    let temp = ''; 
-
-    //
-
-    return temp; 
-}
-
-console.log(dashWord('Semangat!', 3))
-console.log(dashWord('Uzumaki Naruto', 2)) // Uz-um-ak-i -Na-ru-to
-console.log(dashWord('081234567890', 4)) // 0812-3456-7890
-console.log(dashWord('Indonesia Hebat', 10)) // Indonesia -Hebat
-console.log(dashWord('Love Coding', 1)) // L-o-v-e- -C-o-d-i-n-g
+// fn palindrome
+// 123 === 321 = false
+// 124 === 421 = false
+// ...
+// 131 === 131 = true1,11,
